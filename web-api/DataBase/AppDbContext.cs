4c2443c181735365a2e85267674ceefdc1e23f1a
luxiag <luxiag@qq.com>
2024-06-23 21:51:29 +0800

    'feat:添加efc'

---
+++ web-api/DataBase/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api.Modules;
+
+namespace web_api.DataBase
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<TouristRoute> TouristRoutes { get; set; }
+        public DbSet<TouristRoutePicture> touristRoutePictures { get; set; }
+    }
+}
--- web-api/Program.cs
+++ web-api/Program.cs
@@ -1,5 +1,8 @@
 
 using web_api.Services;
+using Microsoft.EntityFrameworkCore;
+using web_api.DataBase;
+using Microsoft.Extensions.Configuration;
 
 namespace web_api
 {
@@ -9,17 +12,30 @@ namespace web_api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var app = builder.Build();
+
+            var config = app.Configuration;
             // Add services to the container.
 
             builder.Services.AddControllers();
 
-            builder.Services.AddTransient<ITouristRouteRepository, MockTourisRouteRepository>();
+            //builder.Services.AddTransient<ITouristRouteRepository, MockTourisRouteRepository>();
+            builder.Services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
+
+
+            builder.Services.AddDbContext<AppDbContext>(option =>
+            {
+                //SQLService≈‰÷√
+                //option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FakeXiechengDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                //option.UseSqlServer("server=localhost; Database=FakeXiechengDb; User Id=sa; Password=PaSSword12!;");
+                option.UseSqlServer(config["DbContext:ConnectionString"]);
+            });
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -34,7 +50,7 @@ namespace web_api
 
 
             app.MapControllers();
-
+             
             app.Run();
         }
     }
